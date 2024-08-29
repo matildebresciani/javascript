a707dd988d625006e942422e91d7ff33b3509b82
matilde <mabr0005@stud.kea.dk>
2024-08-29 09:00:02 +0200

    øvelse 3

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Øvelse 1
+
 function checkDrink() {
     if (this.alt === 'sodavand') {
       console.log('Alkoholfri');
@@ -11,3 +13,28 @@ const drinks = document.querySelectorAll("img");
 drinks.forEach(function(drink) {
   drink.addEventListener('click', checkDrink);
 });
+
+// Øvelse 2
+
+// Øvelse 3
+
+
+document.querySelector('button').addEventListener('click', tjekTal);
+
+let forsoeg = 0;
+const ranNum = Math.floor(Math.random() * 11);
+
+function tjekTal() {
+    forsoeg++;
+    const brugerTal = document.querySelector("#tal").value;
+
+    if(ranNum > brugerTal) {
+        alert("Øv! " + brugerTal + " var for lavt. Prøv igen :)");
+    } else if (ranNum < brugerTal) {
+        alert("Øv! " + brugerTal + " var for højt. Prøv igen :)")
+    } else {
+        alert(brugerTal + " er rigtigt!" + " Du brugte " + forsoeg + " forsøg. Prøv igen med et nyt tal :)");
+        location.reload();
+    }
+}
+
