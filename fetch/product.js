3cbc55f9b4b88549b5f417d967dfa8b359bfaef4
matilde <mabr0005@stud.kea.dk>
2024-08-30 11:30:18 +0200

    fetchh

--- fetch/product.js
+++ fetch/product.js
@@ -1,4 +1,15 @@
 window.addEventListener("DOMContentLoaded", init);
 
 const id = 1163
-const
+const uri = `https://kea-alt-del.dk/t7/api/products/${id}`;
+
+function init() {
+    fetch(uri).then((reponse) => reponse.json()).then(handleData);
+}
+
+function handleData(json) {
+console.log("json", json);
+
+document.querySelector("h1").textContent = json.productdisplayname;
+document.querySelector("p").textContent = json.brandbio;
+}
--- fetch/script.js
+++ fetch/script.js
@@ -1,3 +1,27 @@
 window.addEventListener("DOMContentLoaded", init);
 
-const productsURI = "
+const productsURI = "https://kea-alt-del.dk/t7/api/products";
+
+let productList;
+let productTemplate;
+
+function init() {
+    productList = document.querySelector("#product_list");
+    console.log("productList", productList);
+    productTemplate = document.querySelector("template").content;
+    console.log("productTemplate", productTemplate);
+
+    fetch(productsURI).then((response) => {console.log("response", response); return response.json();}).then(logJSON);
+}
+
+function logJSON(json) {
+console.log("json", json);
+json.forEach(showproduct);
+}
+
+function showproduct(product) {
+    const clone = productTemplate.cloneNode(true);
+    clone.querySelector("h3").textContent = product.name + " " + product.productdisplayname;
+    clone.querySelector("p").textContent = product.season;
+    productList.appendChild(clone);
+}
